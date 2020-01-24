6adb827dfb671a56851069b12bbb7a8ac731e1cf
Anto <ganto1812@gmail.com>
2020-01-24 19:48:54 -0300

    displayed more details, added error alerts

--- src/components/projects/react/weather/Weather.js
+++ src/components/projects/react/weather/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import weather from './Weather.png';
+import { Alert, Button } from 'react-bootstrap';
 
 export default function Weather() {
 	const [temperature, setTemperature] = useState('');
@@ -7,26 +8,57 @@ export default function Weather() {
 	const [country, setCountry] = useState('');
 	const [humidity, setHumidity] = useState('');
 	const [description, setDescription] = useState('');
+	const [feelsLike, setFeelsLike] = useState('');
+	const [minTemp, setMinTemp] = useState('');
+	const [maxTemp, setMaxTemp] = useState('');
 	const [, setError] = useState('');
+	const [showAlert, setShowAlert] = useState(false);
+	const [alertText, setAlertText] = useState('');
 
+	// TODO: Remove API!
 	const API_KEY = '5e24ee893ffee0be84871d9545bb6d57';
 
 	const getWeather = async e => {
 		e.preventDefault();
 		const city = e.target.elements.city.value;
 		const country = e.target.elements.country.value;
-		const api_call = await fetch(
-			`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
-		);
-		const data = await api_call.json();
-		console.log(data);
+		document.getElementById('getWeather').reset();
 
-		setTemperature(data.main.temp);
-		setCity(data.name);
-		setCountry(data.sys.country);
-		setHumidity(data.main.humidity);
-		setDescription(data.weather[0].description);
-		setError('');
+		if (!city && !country) {
+			{
+				setAlertText('Oh no! Type in something');
+				setShowAlert(true);
+			}
+		} else {
+			setShowAlert(false);
+			try {
+				const api_call = await fetch(
+					`https://api.openweathermap.org/data/2.5/weather?q=${city}${city &&
+						country &&
+						','}${country}&units=metric&appid=${API_KEY}`
+				);
+				if (api_call.ok) {
+					const data = await api_call.json();
+					setTemperature(data.main.temp);
+					setCity(data.name);
+					setCountry(data.sys.country);
+					setHumidity(data.main.humidity);
+					setDescription(data.weather[0].description);
+					setFeelsLike(data.main.feels_like);
+					setMinTemp(data.main.temp_min);
+					setMaxTemp(data.main.temp_max);
+					setError('');
+				} else {
+					const data = await api_call.json();
+					console.log('data:' + data);
+					setAlertText(data.message);
+					setShowAlert(true);
+				}
+			} catch (err) {
+				setAlertText('Something went wrong. Try again later');
+				setShowAlert(true);
+			}
+		}
 	};
 
 	return (
@@ -34,20 +66,60 @@ export default function Weather() {
 			<img src={weather} alt='weather' className='mx-auto' />
 			<h1>Weather APP</h1>
 			<p>Find out weather temperature, conditions and more..</p>
-			<form onSubmit={getWeather}>
+			<form onSubmit={getWeather} id='getWeather'>
 				<input type='text' name='city' placeholder='City...' />
 				<input type='text' name='country' placeholder='Country...' />
-				<button type='submit'>Get Weather</button>
+				<Button type='submit' variant='info'>
+					Get Weather
+				</Button>
+				{showAlert ? (
+					<Alert
+						variant='danger'
+						onClose={() => setShowAlert(false)}
+						dismissible
+					>
+						<Alert.Heading className='text-capitalize'>
+							{alertText}
+						</Alert.Heading>
+					</Alert>
+				) : (
+					''
+				)}
 			</form>
-			<h2>
-				{city}, {country}
-			</h2>
-			<h3>Current temperature:</h3>
-			<p>{temperature}</p>
-			<h3>Current humidity:</h3>
-			<p>{humidity}</p>
-			<h3>Description: </h3>
-			<p>{description}</p>
+			<div style={{ display: city || country ? 'block' : 'none' }}>
+				<h1 className='my-5 pb-5'>
+					{city}, {country}
+				</h1>
+				<div
+					className='row col-10 mx-auto mb-5'
+					style={{ fontFamily: 'Arial' }}
+				>
+					<div className='col-12 mx-auto col-sm-4'>
+						<h2 className='btn-info p-3 container-fluid'>Temperature:</h2>
+						<p>{temperature}C</p>
+					</div>
+					<div className='col-12 mx-auto col-sm-4'>
+						<h2 className='btn-info p-3 container-fluid'>Humidity:</h2>
+						<p>{humidity}%</p>
+					</div>
+					<div className='col-12 mx-auto col-sm-4'>
+						<h2 className='btn-info p-3 container-fluid'>Description:</h2>
+						<p className='text-capitalize'>{description}</p>
+					</div>
+					<div className='col-12 mx-auto col-sm-4'>
+						<h2 className='btn-info p-3 container-fluid'>Feels like: </h2>
+						<p>{feelsLike}C</p>
+					</div>
+					<div className='col-12 mx-auto col-sm-4'>
+						<h2 className='btn-info p-3 container-fluid'>Min Temp: </h2>
+						<p>{minTemp}C</p>
+					</div>
+					<div className='col-12 mx-auto col-sm-4'>
+						<h2 className='btn-info p-3 container-fluid'>Max Temp: </h2>
+						<p>{maxTemp}C</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
