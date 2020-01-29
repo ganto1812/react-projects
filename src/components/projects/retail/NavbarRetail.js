dedba0537669d06dfd99113889d85beedd834c72
Anto <ganto1812@gmail.com>
2020-01-29 17:44:22 -0300

    changed position and icon for shopping cart

--- src/components/projects/retail/NavbarRetail.js
+++ src/components/projects/retail/NavbarRetail.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 export default function StoreNavbar() {
 	return (
@@ -8,24 +10,19 @@ export default function StoreNavbar() {
 			<Navbar.Brand className='navbar-brand'>WELCOME TO MY STORE</Navbar.Brand>
 			<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 			<Navbar.Collapse id='responsive-navbar-nav'>
-				<Nav className=''>
-					<ul className='navbar-nav mx-auto mt-2 mt-lg-0'>
-						<Button className='nav-item' variant='outline-secondary'>
-							<Link to='/projects/2' className='nav-link'>
-								Products
-							</Link>
-						</Button>
-						<Button className='nav-item' variant='outline-secondary'>
-							<Link className='nav-link' to='/projects/2/cart'>
-								<i
-									className='fa fa-shopping-cart text-white'
-									aria-hidden='true'
-								></i>
-							</Link>
-						</Button>
-					</ul>
+				<Nav className='navbar-nav'>
+					<Button className='nav-item' variant='outline-secondary'>
+						<Link to='/projects/2' className='nav-link'>
+							Products
+						</Link>
+					</Button>
 				</Nav>
 			</Navbar.Collapse>
+			<Button className='nav-item' variant='outline-secondary'>
+				<Link className='nav-link' to='/projects/2/cart'>
+					<FontAwesomeIcon icon={faShoppingCart} color='#fff'></FontAwesomeIcon>
+				</Link>
+			</Button>
 		</Navbar>
 	);
 }
