a0be840e3d80268e0164678ea16d67203be76019
Anto <ganto1812@gmail.com>
2020-01-29 20:23:50 -0300

    Projects cards with info and imgs

--- src/components/sections/Card.js
+++ src/components/sections/Card.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Projects from './Projects';
 // import ./;
 
-export default function Card() {
-	return (
-		<div className="flip-card mx-auto">
-			<div className="flip-card-inner">
-				<div className="flip-card-front">
-					<img src="img_avatar.png" alt="Avatar" />
+export default function Card(props) {
+	const { id, title, info, img } = props.projects;
+	return props.projects.map(project => (
+		<div className='flip-card mx-auto'>
+			<div className='flip-card-inner'>
+				<div className='flip-card-front'>
+					<img
+						src={'/images/projects/' + project.img}
+						className='img-fluid'
+						alt='Project'
+					/>
 				</div>
-				<div className="flip-card-back">
-					<h1>John Doe</h1>
-					<p>Architect & Engineer</p>
-					<p>We love that guy</p>
+				<div className='flip-card-back'>
+					<h1>{project.title}</h1>
+					<p>{project.info}</p>
 				</div>
 			</div>
 		</div>
-	)
+	));
 }
--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -3,6 +3,29 @@ import Section from './Section';
 import Card from './Card';
 
 export default function Projects() {
+	const projects = [
+		{
+			id: 1,
+			title: 'Weather App',
+			info:
+				'An app that searches the weather conditions of an specific country or city in real time',
+			img: 'weather.png'
+		},
+		{
+			id: 2,
+			title: 'Retail Phone Store',
+			info:
+				'A phone retail shop with incorporated cart and paypal transactions',
+			img: 'retail.png'
+		},
+		{
+			id: 3,
+			title: 'To do App',
+			info:
+				'An app that lets you add anything to do the list. With get and fetch real time actions from an API',
+			img: 'todo.png'
+		}
+	];
 	return (
 		<div>
 			<Section sectionName='My Projects' sectionId='projects' key='2'></Section>
@@ -10,9 +33,13 @@ export default function Projects() {
 			<p>Take a look!</p>
 			<div>
 				<h2>React Projects</h2>
-				<Card></Card>
+				<div className='row'>
+					<Card projects={projects}></Card>
+				</div>
 			</div>
-			<div><h2>Vanilla JS Projects</h2></div>
+			<div>
+				<h2>Vanilla JS Projects</h2>
+			</div>
 		</div>
 	);
 }
