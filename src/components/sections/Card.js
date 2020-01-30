7a1d89acb1646c851c23ca54837d9653d195d587
Anto <ganto1812@gmail.com>
2020-01-30 18:17:15 -0300

    project cards' path updated

--- src/components/sections/Card.js
+++ src/components/sections/Card.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import Projects from './Projects';
-// import ./;
+import { Link } from 'react-router-dom';
 
 export default function Card(props) {
 	const { id, title, info, img } = props.projects;
 	return props.projects.map(project => (
 		<div className='flip-card mx-auto'>
-			<div className='flip-card-inner'>
-				<div className='flip-card-front'>
-					<img
-						src={'/images/projects/' + project.img}
-						className='img-fluid'
-						alt='Project'
-					/>
+			<Link to={'/projects/' + project.id}>
+				<div className='flip-card-inner'>
+					<div className='flip-card-front'>
+						<img
+							src={'/images/projects/' + project.img}
+							className='img-fluid'
+							alt='Project'
+						/>
+					</div>
+					<div className='flip-card-back'>
+						<h1 className='pt-3'>{project.title}</h1>
+						<p>{project.info}</p>
+					</div>
 				</div>
-				<div className='flip-card-back'>
-					<h1>{project.title}</h1>
-					<p>{project.info}</p>
-				</div>
-			</div>
+			</Link>
 		</div>
 	));
 }
