73e1d597b39a8b101c79fa041f757a7aeabe2876
Anto <ganto1812@gmail.com>
2020-02-14 14:50:02 +0000

    retail navbar responsive

--- src/components/projects/retail/NavbarRetail.js
+++ src/components/projects/retail/NavbarRetail.js
@@ -7,22 +7,25 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 export default function StoreNavbar() {
 	return (
 		<Navbar collapseOnSelect expand='md' bg='dark' variant='dark'>
-			<Navbar.Brand className='navbar-brand'>WELCOME TO MY STORE</Navbar.Brand>
+			<Navbar.Brand>WELCOME TO MY STORE</Navbar.Brand>
 			<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 			<Navbar.Collapse id='responsive-navbar-nav'>
-				<Nav className='navbar-nav'>
+				<Nav className='mr-auto'>
 					<Button className='nav-item' variant='outline-secondary'>
 						<Link to='/projects/2' className='nav-link'>
 							Products
 						</Link>
 					</Button>
+					<Button className='nav-item' variant='outline-secondary'>
+						<Link className='nav-link' to='/projects/2/cart'>
+							<FontAwesomeIcon
+								icon={faShoppingCart}
+								color='#fff'
+							></FontAwesomeIcon>
+						</Link>
+					</Button>
 				</Nav>
 			</Navbar.Collapse>
-			<Button className='nav-item' variant='outline-secondary'>
-				<Link className='nav-link' to='/projects/2/cart'>
-					<FontAwesomeIcon icon={faShoppingCart} color='#fff'></FontAwesomeIcon>
-				</Link>
-			</Button>
 		</Navbar>
 	);
 }
