036fe56fc1d99d2b5215fcc334667d665269fce7
Anto P <ganto1812@gmail.com>
2021-01-07 21:09:14 +0000

    back to ng portfolio btn

--- src/components/sections/Section.js
+++ src/components/sections/Section.js
@@ -5,6 +5,9 @@ import spadeOnly from './spadeonly.svg';
 export default function Section({ sectionName }) {
 	return (
 		<div className='section'>
+			<a href="https://ana-portfolio-2021.netlify.app">
+			<img src="/images/about/name.svg" alt="icon" className="text-center d-inline my-3" width="200"/>
+			</a>
 			<img
 				src={spadeOnly}
 				id='sectionSpade1'
